1ddc2bd4306be93d1b558fa02adf807ebd0861d0
Oliver Naslund <oliver.naslund.7130@student.uu.sem>
2023-05-16 12:27:43 +0200

    mod

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
-
 from person import Person
-#import fib*
-#import fib_numba
+import matplotlib.pyplot as plt
+import numba
+from numba import njit
+
+
+@njit
+def fib_numba(n):
+    if n <= 1:
+        return n
+    else:
+        return fib_numba(n-1) + fib_numba(n-2)
+    
+
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
 
 def main():
 	f = Person(50)
-	print(f.getAge())
-	print(f.getDecades())
-
-	f.setAge(51)
-	print(f.getAge())
-	print(f.getDecades())
+	#print(f.getAge())
+	#print(f.getDecades())
+    #    
+	#f.setAge(51)
+	#print(f.getAge())
+	#print(f.getDecades())
 
 	f.setAge(10)
 	print(f.fib())
@@ -20,5 +36,6 @@ def main():
 	print(f.fib())
 
 if __name__ == '__main__':
-	main()
+	#main()
+	
 
